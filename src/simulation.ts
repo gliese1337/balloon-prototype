dcb3d945923fe0659a4620c7f0eaea5e9e0daf13
Logan Kearsley <chronosurfer@gmail.com>
2020-09-21 22:24:09 -0600

    Separate calculations to write to a single buffer in each step

--- src/simulation.ts
+++ src/simulation.ts
@@ -14,16 +14,91 @@ function createVectors(size: number) {
   return buffer;
 }
 
-function stream(i: number, iq: number, barriers: boolean[], destination: (i: number, j: number) => number, collided: Float32Array, streamed: Float32Array) {
-  if (barriers[i]) {
-    // Handle bounce-back from barriers
-    for (let j=1;j<q;j++) {
-      streamed[destination(i, j)] = collided[iq + opp[j]];
+function update_rho(max: number, rho: Float32Array, streamed: Float32Array) {
+  for (let i=0,iq=0; i<max; i++,iq+=q) {
+    let newrho = streamed[iq];  // macroscopic density
+    for (let j = 1; j < q; j++) {
+      const v = streamed[iq+j];
+      newrho += v;
     }
-  } else {
-    // Move particles along their velocity vector
-    for (let j=1;j<q;j++) {
-      streamed[destination(i, j)] = collided[iq + j];
+
+    // hack for stability
+    rho[i] = newrho <= 0 ? 0.01 : newrho;
+  }
+}
+
+function update_velocities(max: number, rho: Float32Array, velocities: Float32Array, streamed: Float32Array) {
+  for (let i=0,iv=0,iq=0; i<max; i++,iq+=q,iv+=3) {
+    let ux = 0, uy = 0, uz = 0; // macroscopic velocity components
+    for (let j = 1; j < q; j++) {
+      const v = streamed[iq+j];
+      ux += cxs[j]*v;
+      uy += cys[j]*v;
+      uz += czs[j]*v;
+    }
+
+    const newrho = rho[i];
+    velocities[iv+0] = ux / newrho;
+    velocities[iv+1] = uy / newrho;
+    velocities[iv+2] = uz / newrho;
+  }
+}
+
+function collide(max: number, viscosity: number, rho: Float32Array, velocities: Float32Array, collided: Float32Array, streamed: Float32Array) {
+  const tau = 3*viscosity + 0.5; // relaxation timescale
+  const omega = 1 / tau;
+  const invomega = 1 - omega;
+
+  for (let i=0,iv=0,iq=0; i<max; i++,iq+=q,iv+=3) {
+    const ux = velocities[iv];
+    const uy = velocities[iv+1];
+    const uz = velocities[iv+2];
+    const newrho = rho[i];
+
+    /* Calculate collisions */
+
+    /*
+    f_j^eq = w_j rho (1 + (e_j|u) / c_s^2 + (e_j|u)^2/(2 c_s^4) - u^2/(2 c_s^2))
+    f`_j = omega f_j^eq + (1 - omega) f_j 
+
+    c_s, the lattice speed of sound, is 1/sqrt(3)
+    Thus, 1/c_s^2 = 3
+          1/(2 c_s^4) = 4.5
+          1/(2 c_s^2) = 1.5
+    */
+
+    const u2 =  1 - 1.5 * (ux * ux + uy * uy + uz * uz);
+    for (let j = 1; j < q; j++) {
+      const dir = cxs[j]*ux + cys[j]*uy + czs[j]*uz;
+      const eq = weights[j] * newrho * (u2 + 3 * dir + 4.5 * dir * dir);
+      collided[iq+j] = omega * eq + invomega * streamed[iq+j];
+    }
+  }
+}
+
+function stream(xdim: number, ydim: number, max: number, viscosity: number, rho: Float32Array, barriers: boolean[], velocities: Float32Array, collided: Float32Array, streamed: Float32Array) {
+  const tau = 3*viscosity + 0.5; // relaxation timescale
+  const omega = 1 / tau;
+  const invomega = 1 - omega;
+
+  for (let i=0,iv=0,iq=0; i<max; i++,iq+=q,iv+=3) {
+    const ux = velocities[iv];
+    const uy = velocities[iv+1];
+    const uz = velocities[iv+2];
+
+    const u2 =  1 - 1.5 * (ux * ux + uy * uy + uz * uz);
+    streamed[iq] = omega * weights[0] * rho[i] * u2 + invomega * streamed[iq];
+    
+    if (barriers[i]) {
+      // Handle bounce-back from barriers
+      for (let j=1;j<q;j++) {
+        streamed[q*((i+(cxs[j]+(cys[j]+czs[j]*ydim)*xdim)+max)%max)+j] = collided[iq + opp[j]];
+      }
+    } else {
+      // Move particles along their velocity vector
+      for (let j=1;j<q;j++) {
+        streamed[q*((i+(cxs[j]+(cys[j]+czs[j]*ydim)*xdim)+max)%max)+j] = collided[iq + j];
+      }
     }
   }
 }
@@ -31,73 +106,24 @@ function stream(i: number, iq: number, barriers: boolean[], destination: (i: num
 export default class LatticeBoltzmann {        
   private streamed: Float32Array; // microscopic densities along each lattice direction
   private collided: Float32Array;
-  public rho: Float32Array;    // macroscopic density; cached for rendering
+  public rho: Float32Array;        // macroscopic density; cached for rendering
+  public velocities: Float32Array; // macroscopic velocity
 
   constructor(public xdim: number, public ydim: number, public zdim: number) {
     const size = xdim * ydim * zdim;
     this.streamed = createVectors(size);
     this.collided = createVectors(size);
     this.rho = new Float32Array(size);
+    this.velocities = new Float32Array(size*3);
   }
 
   public step(viscosity: number, barriers: boolean[]) {
-    const { xdim, ydim, zdim, rho, collided, streamed } = this;
+    const { xdim, ydim, zdim, rho, velocities, collided, streamed } = this;
     const max = xdim * ydim * zdim;
-
-    /* Collision Loop */
-
-    for (let i=0,iq=0; i<max; i++,iq+=q) {
-      /* Calculate macroscopic quantities */
-
-      let newrho = streamed[iq];  // macroscopic density
-      let ux = 0, uy = 0, uz = 0; // macroscopic velocity components
-      for (let j = 1; j < q; j++) {
-        const v = streamed[iq+j];
-        newrho += v;
-        ux += cxs[j]*v;
-        uy += cys[j]*v;
-        uz += czs[j]*v;
-      }
-
-      // hack for stability
-      if (newrho <= 0) newrho = 0.01;
-
-      rho[i] = newrho;
-      ux/=newrho;
-      uy/=newrho;
-      uz/=newrho;
-
-      /* Calculate collisions */
-
-      /*
-      f_j^eq = w_j rho (1 + (e_j|u) / c_s^2 + (e_j|u)^2/(2 c_s^4) - u^2/(2 c_s^2))
-      f`_j = omega f_j^eq + (1 - omega) f_j 
-
-      c_s, the lattice speed of sound, is 1/sqrt(3)
-      Thus, 1/c_s^2 = 3
-            1/(2 c_s^4) = 4.5
-            1/(2 c_s^2) = 1.5
-      */
-
-      const tau = 3*viscosity + 0.5; // relaxation timescale
-      const omega = 1 / tau;
-      const invomega = 1 - omega;
-
-      const u2 =  1 - 1.5 * (ux * ux + uy * uy + uz * uz);
-      streamed[iq] = omega * weights[0] * newrho * u2 + invomega * streamed[iq];
-      for (let j = 1; j < q; j++) {
-        const dir = cxs[j]*ux + cys[j]*uy + czs[j]*uz;
-        const eq = weights[j] * newrho * (u2 + 3 * dir + 4.5 * dir * dir);
-        collided[iq+j] = omega * eq + invomega * streamed[iq+j];
-      }
-    }
-
-    const destination = (i: number, j: number) =>
-                      q*((i+(cxs[j]+(cys[j]+czs[j]*ydim)*xdim)+max)%max)+j;
-
-    for (let i=0,iq=0; i<max; i++,iq+=q) {
-      stream(i, iq, barriers, destination, collided, streamed);
-    }
+    update_rho(max, rho, streamed);
+    update_velocities(max, rho, velocities, streamed);
+    collide(max, viscosity, rho, velocities, collided, streamed);
+    stream(xdim, ydim, max, viscosity, rho, barriers, velocities, collided, streamed);
   }
 
   // Set all densities in a cell to their equilibrium values for a given velocity and density:
