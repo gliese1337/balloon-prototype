845331bb5b96cc9c986cfd5de680c80ba9cc0eb6
Logan Kearsley <chronosurfer@gmail.com>
2021-03-26 16:27:40 -0600

    Separate stationary plane

--- src/simulation.ts
+++ src/simulation.ts
@@ -1,3 +1,4 @@
+const w0 = 4/9;
 const weights = [4/9, 1/9, 1/9, 1/9, 1/9, 1/36, 1/36, 1/36, 1/36];
 const c: [number, number][] = [
   [ 0,  0 ],
@@ -11,9 +12,9 @@ const opp = [ 0, 2, 1, 4, 3, 6, 5, 8, 7 ];
 
 const q = 9;
 
-function update_macros(max: number, streamed: Float32Array, macros: Float32Array) {
+function update_macros(max: number, stationary: Float32Array, streamed: Float32Array, macros: Float32Array) {
   for (let i = 0, im = 0; i < max; i++, im += 3) {
-    let rho = streamed[i];  // macroscopic density
+    let rho = stationary[i];  // macroscopic density
     let ux = 0, uy = 0; // macroscopic velocity components
     for (let j = 1, plane = max; j < q; j++, plane += max) {
       const v = streamed[plane + i];
@@ -48,12 +49,12 @@ function collide(
   }
 }
 
-function stationary(max: number, omega_w: number, invomega: number, macros: Float32Array, streamed: Float32Array) {
+function update_static(max: number, omega_w: number, invomega: number, macros: Float32Array, stationary: Float32Array) {
   for (let i = 0, im = 0; i < max; i++, im += 3) {
     const ux = macros[im + 1];
     const uy = macros[im + 2];
     const u2 =  1 - 1.5 * (ux * ux + uy * uy);
-    streamed[i] = omega_w * macros[im] * u2 + invomega * streamed[i];
+    stationary[i] = omega_w * macros[im] * u2 + invomega * stationary[i];
   }
 }
 
@@ -70,13 +71,16 @@ function stream(
   }
 }
 
-export default class LatticeBoltzmann {        
+export default class LatticeBoltzmann {
+  private stationary: Float32Array;
   private streamed: Float32Array; // microscopic densities along each lattice direction
   private collided: Float32Array;
   public macros: Float32Array;    // macroscopic density & velocity
 
   constructor(public readonly xdim: number, public readonly ydim: number) {
     const size = xdim * ydim;
+
+    this.stationary = new Float32Array(size).fill(w0);
 
     const salloc = new ArrayBuffer(size * q << 2);
     for (let i = 0; i < q; i++)
@@ -88,20 +92,20 @@ export default class LatticeBoltzmann {
   }
 
   public step(viscosity: number, barriers: boolean[]) {
-    const { xdim, ydim, macros, collided, streamed } = this;
+    const { xdim, ydim, macros, collided, stationary, streamed } = this;
     const max = xdim * ydim;
 
     const tau = 3*viscosity + 0.5; // relaxation timescale
     const omega = 1 / tau;
     const invomega = 1 - omega;
 
-    update_macros(max, streamed, macros);
+    update_macros(max, stationary, streamed, macros);
     for (let j = 1, plane = max; j < q; j++, plane += max) {
       const omega_w = omega * weights[j];
       collide(max, omega_w, invomega, c[j], plane, macros, streamed, collided);
     }
 
-    stationary(max, omega * weights[0], invomega, macros, streamed);
+    update_static(max, omega * w0, invomega, macros, stationary);
     for (let j = 1, plane = max; j < q; j++, plane += max) {
       const opp_plane = max * opp[j];
       stream(xdim, max, c[j], plane, opp_plane, barriers, collided, streamed);
@@ -116,7 +120,9 @@ export default class LatticeBoltzmann {
     this.macros[i * 3] = rho;
 
     const u2 =  1 - 1.5 * (ux * ux + uy * uy);
-    for (let j = 0, plane = 0; j < q; j++, plane += max) {
+    
+    this.stationary[i] = w0 * rho * u2;
+    for (let j = 1, plane = max; j < q; j++, plane += max) {
       const dir = c[j][0]*ux + c[j][1]*uy;
       streamed[plane + i] = weights[j] * rho * (u2 + 3 * dir + 4.5 * dir * dir);
     }
